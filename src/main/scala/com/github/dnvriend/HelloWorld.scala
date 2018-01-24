33d5082495640ad8493e7c240d60ec7eb638c038
Dennis Vriend <dnvriend@gmail.com>
2018-01-24 20:20:25 +0100

    more config and closing pool

--- src/main/scala/com/github/dnvriend/HelloWorld.scala
+++ src/main/scala/com/github/dnvriend/HelloWorld.scala
@@ -1,30 +1,36 @@
 package com.github.dnvriend
 
-import java.util.Properties
-
 import anorm._
-import com.typesafe.config.{ Config, ConfigFactory }
-import com.zaxxer.hikari.HikariConfig
-import com.zaxxer.hikari.HikariDataSource
+import com.typesafe.config.{Config, ConfigFactory}
+import com.zaxxer.hikari.{HikariConfig, HikariDataSource}
 
 object DB {
-  def toProps(config: Config): Properties = {
+  def toProps(config: Config): java.util.Properties = {
     val properties = new java.util.Properties
     config.entrySet.forEach((e) => properties.setProperty(e.getKey, config.getString(e.getKey)))
     properties
   }
+
   val config: HikariConfig = new HikariConfig(toProps(ConfigFactory.load().getObject("db").toConfig))
   val ds: HikariDataSource = new HikariDataSource(config)
+
   def withConnection[A](f: java.sql.Connection => A): A = {
     val conn: java.sql.Connection = ds.getConnection
     try f(conn) finally conn.close()
   }
+
+  /**
+    * Shutdown the DataSource and its associated pool.
+    */
+  def shutdown() = ds.close()
 }
 
 object Person {
   implicit val parser: RowParser[Person] = Macro.namedParser[Person]
 }
+
 final case class Person(name: String, age: Int)
+
 object HelloWorld extends App {
   DB.withConnection { implicit conn =>
     SQL"""
@@ -32,12 +38,14 @@ object HelloWorld extends App {
        name VARCHAR,
        age NUMERIC
       )
-    """ executeUpdate ()
+    """ executeUpdate()
 
-    SQL"INSERT INTO person values ('dennis', 43)" executeUpdate ()
+    SQL"INSERT INTO person values ('dennis', 43)" executeUpdate()
 
     val xs: Seq[Person] = SQL"SELECT * FROM PERSON".as(Person.parser.*)
     xs foreach println
   }
+
+  DB.shutdown()
 }
 
